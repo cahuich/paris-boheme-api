9aeac125b212cbf7ebe3d4cfdfb90496d3964875
cahuich <pjosephrose@gmail.com>
2026-02-15 16:43:29 -0500

    Modificacion cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,1 +1,7 @@
 export const FRONTEND_URL = 'https://paris-boheme.vercel.app';
+import cors from 'cors';
+
+export const corsMiddleware = cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true
+});
